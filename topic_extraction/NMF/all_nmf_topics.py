88abc5305f5a7c3382cdcc363142a48d02dcf0d5
Teemu Sidoroff <tsido@iki.fi>
2023-12-29 19:52:27 +0200

    NMF appendix table creation

---
+++ topic_extraction/NMF/all_nmf_topics.py
@@ -0,0 +1,56 @@
+
+nmf = {0: 'game center simple best playing',
+ 1: 'word words letters letter search',
+ 2: 'solitaire cards card klondike spider',
+ 3: 'kids learning fun educational games',
+ 4: 'subscription period account renewal gameclub',
+ 5: 'puzzles puzzle jigsaw pieces solve',
+ 6: 'fish big games discover enjoy',
+ 7: 'play player players online multiplayer',
+ 8: 'battle enemies enemy war weapons',
+ 9: 'car racing cars race tracks',
+ 10: 'iphone ipad touch ipod universal',
+ 11: 'english spanish french german languages',
+ 12: 'tiles mahjong tile board remove',
+ 13: 'slots casino slot vegas win',
+ 14: 'numbers number math brain color',
+ 15: 'chess moves board pieces games',
+ 16: 'guess friends facebook share challenge',
+ 17: 'escape room objects solve download',
+ 18: 'levels level difficulty challenging complete',
+ 19: 'ball balls bowling physics soccer',
+ 20: 'bubble bubbles pop shooter popping',
+ 21: 'sudoku notes grid column row',
+ 22: 'questions trivia quiz knowledge answer',
+ 23: 'truck garbage trucks monster vehicles',
+ 24: 'adventure story island characters mysterious',
+ 25: 'animals animal farm zoo sounds',
+ 26: 'blocks block color match puzzle',
+ 27: 'dice roll rolls board rolling',
+ 28: 'fun great graphics music addictive',
+ 29: 'poker texas chips hold video',
+ 30: 'zombies zombie weapons undead survive',
+ 31: 'tac tic toe player row',
+ 32: 'tap screen button left right',
+ 33: 'mode modes challenge time arcade',
+ 34: 'children child learning parents learn',
+ 35: 'para que cartas com del',
+ 36: 'hidden object objects cradle city',
+ 37: 'score points high scores bonus',
+ 38: 'new games unlock create best',
+ 39: 'christmas santa presents holiday time',
+ 40: 'app free real money purchases',
+ 41: 'tabtale privacy app policy limited',
+ 42: 'und die der med des',
+ 43: 'jump run coins obstacles collect',
+ 44: 'world compete real best countries',
+ 45: 'baby dress pet little hair'}
+
+
+print("\\scriptsize")
+print("\\begin{tabular}{|c|c|}")
+print("\hline")
+for (k, v) in nmf.items():
+  print((k+1), "&", v, "\\\\")
+  print("\hline")
+print("\end{tabular}")
